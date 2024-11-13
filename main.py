c86a09cfc071037c5c3e4c3d4f965a8737fcd0ba
embed-Rayn <57181674+embed-Rayn@users.noreply.github.com>
2024-11-13 22:06:55 +0900

    update main function

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def date_minus_day_to_str(date_obj, days = 1, str_format = "%Y년 %m월 %d일"):
     formatted_date = before_oneday_obj.strftime(str_format)
     return formatted_date
 
+def das():
+    return chr
 ####################################################
 data_xls_name = "data/20241105_data.xlsx"
 workbook = openpyxl.load_workbook(data_xls_name)
@@ -18,38 +20,78 @@ for row_idx, row in enumerate(sheet):
     row_data = [col.value for col in row]
     row_values.append(row_data)
 
-date_list = []
-campaign_list = []
-exposure_num_list = []
-click_num_list = []
-advertisement_list = []
+dataset = {}
 for row in row_values:
-    date_list.append(str(int(row[0])))
-    campaign_list.append(row[6])
-    exposure_num_list.append(int(row[7]))
-    click_num_list.append(int(row[8]))
-    advertisement_list.append(int(row[9]))
+    date_obj = datetime.strptime(str(int(row[0])), "%Y%m%d").date()
+    temp = {}
+    temp["campaign"] = row[6]
+    temp["exposure_num"] = row[7]
+    temp["click_num"] = row[8]
+    temp["ad_cost"] = row[9]
+    temp["convert_num"] = row[15]
+    temp["convert_cost"] = row[18]
+    try:
+        dataset[date_obj].append(temp)
+    except KeyError:
+        dataset[date_obj] = [temp]
 workbook.close()
 
-assert len(set(date_list)) == 1, "날짜는 하나만..."
-date_obj = datetime.strptime(date_list[0], "%Y%m%d").date()
+report_day = max(dataset.keys())
 #################################################### 리포트 파일
-####################################################["요약"]
+#################################################### ["요약"] 시트
 report_template_xls_name = "data/241106_dearcamp.xlsx"
 report_wb = openpyxl.load_workbook(report_template_xls_name)
 report_sheet = report_wb["요약"]
-date_str = date_minus_day_to_str(date_obj, 1, "%Y년 %m월 %d일")
+date_str = date_minus_day_to_str(report_day, 1, "%Y년 %m월 %d일")
 report_sheet["B31"] = date_str
-date_str = date_minus_day_to_str(date_obj, 1, "%Y-%m-%d")
+date_str = date_minus_day_to_str(report_day, 1, "%Y-%m-%d")
 report_sheet["C104"] = date_str
-####################################################
+#################################################### ["쿠팡_일일"] 시트
 report_sheet = report_wb["쿠팡_일일"]
-date_str = date_minus_day_to_str(date_obj, 1, "%Y년 %m월 %d일")
-report_sheet["B31"] = date_str
-date_str = date_minus_day_to_str(date_obj, 1, "%Y-%m-%d")
-report_sheet["C104"] = date_str
-####################################################
+for date_obj in dataset.keys():
+    before_oneday_obj = date_obj - timedelta(days=1)
+    for row_idx, cell in enumerate(report_sheet["B"]):
+        try:
+            cv_date = cell.value.date()
+        except AttributeError:
+            continue
+        if cv_date == before_oneday_obj:
+            data_idx = row_idx + 1
+            report_sheet[f"D{data_idx}"] = sum(campaign["exposure_num"] for campaign in dataset[date_obj])
+            report_sheet[f"E{data_idx}"] = sum(campaign["click_num"] for campaign in dataset[date_obj])
+            report_sheet[f"H{data_idx}"] = sum(campaign["ad_cost"] for campaign in dataset[date_obj])
+            report_sheet[f"I{data_idx}"] = sum(campaign["convert_num"] for campaign in dataset[date_obj])
+            report_sheet[f"L{data_idx}"] = sum(campaign["convert_cost"] for campaign in dataset[date_obj])
+            # print(report_sheet[f"D{data_idx}"].value, report_sheet[f"E{data_idx}"].value, 
+            #       report_sheet[f"H{data_idx}"].value, report_sheet[f"I{data_idx}"].value, 
+            #       report_sheet[f"L{data_idx}"].value)
+#################################################### ["쿠팡_누적"] 시트
 report_sheet = report_wb["쿠팡_누적"]
+campaign_totals = {}
+for campaigns in dataset.values():
+    for campaign in campaigns:
+        campaign_name = campaign["campaign"]
+        exposure_num = campaign["exposure_num"]
+        click_num = campaign["click_num"]
+        ad_cost = campaign["ad_cost"]
+        convert_num = campaign["convert_num"]
+        convert_cost = campaign["convert_cost"]
+        metric = [exposure_num, click_num, ad_cost, convert_num, convert_cost]
+        if campaign_name in campaign_totals:
+            campaign_totals[campaign_name] = [sum(x) for x in zip(campaign_totals[campaign_name], metric)]
+        else:
+            campaign_totals[campaign_name] = metric
+
+c1 = report_sheet["C9"].value
+metrics = campaign_totals[c1]
+cols = ["C", "D", "E", "H", "I"]
+rows = [9, 10]
+for row in rows:
+    for idx, col in enumerate(cols):
+        cell = report_sheet[f"{col}{row}"]
+        cell = cell.value + metrics[idx]
+
+
 # 변경사항 저장
 report_wb.save(report_template_xls_name)
 report_wb.close()
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta
+import openpyxl
+
 
 # 문자열을 date 객체로 변환
 date_string = "20241105"
@@ -8,35 +10,58 @@ before_oneday_obj = date_obj - timedelta(days=1)
 formatted_date = before_oneday_obj.strftime("%Y년 %m월 %d일")
 print(formatted_date)
 
-# import openpyxl
-# data_xls_name = "data/20241105_data.xlsx"
-# workbook = openpyxl.load_workbook(data_xls_name)
-# sheet = workbook.active  # 기본 활성화된 시트를 선택합니다. 특정 시트가 있으면 sheet = workbook["Sheet1"] 과 같이 지정합니다.
 
-# # 1행의 모든 값 읽기
-# row_values = []
-# for row_idx, row in enumerate(sheet):
-#     if row_idx == 0:
-#         continue
-#     row_data = [col.value for col in row]
-#     row_values.append(row_data)
 
-# date_list = []
-# campaign_list = []
-# exposure_num_list = []
-# click_num_list = []
-# advertisement_list = []
-# for row in row_values:
-#     date_list.append(str(int(row[0])))
-#     campaign_list.append(row[6])
-#     exposure_num_list.append(int(row[7]))
-#     click_num_list.append(int(row[8]))
-#     advertisement_list.append(int(row[9]))
-# workbook.close()
-# print(date_list)
-# print(campaign_list)
-# print(exposure_num_list)
-# print(click_num_list)
-# print(advertisement_list)
-# workbook.close()
-print(len(set([1,1,1,1])))
+data_xls_name = "data/20241105_data.xlsx"
+# data_xls_name = "data/8910.xlsx"
+workbook = openpyxl.load_workbook(data_xls_name)
+sheet = workbook.active  # 기본 활성화된 시트를 선택합니다. 특정 시트가 있으면 sheet = workbook["Sheet1"] 과 같이 지정합니다.
+
+row_values = []
+for row_idx, row in enumerate(sheet):
+    if row_idx == 0:
+        continue
+    row_data = [col.value for col in row]
+    row_values.append(row_data)
+
+dataset = {}
+for row in row_values:
+    date_obj = datetime.strptime(str(int(row[0])), "%Y%m%d").date()
+    temp = {}
+    temp["campaign"] = row[6]
+    temp["exposure_num"] = row[7]
+    temp["click_num"] = row[8]
+    temp["ad_cost"] = row[9]
+    temp["convert_num"] = row[15]
+    temp["convert_cost"] = row[18]
+    try:
+        dataset[date_obj].append(temp)
+    except KeyError:
+        dataset[date_obj] = [temp]
+workbook.close()
+
+print(dataset)
+report_day = max(dataset.keys())
+# date_obj = datetime.strptime(report_day, "%Y%m%d").date()
+date_obj = report_day
+print(date_obj, type(date_obj))
+
+print(sum(campaign["exposure_num"] for campaign in dataset[date_obj]))
+print(campaign["exposure_num"] for campaign in dataset[date_obj])
+
+
+# report_template_xls_name = "data/241106_dearcamp.xlsx"
+# report_wb = openpyxl.load_workbook(report_template_xls_name)
+
+# #########################
+# report_sheet = report_wb["쿠팡_일일"]
+# before_oneday_obj = date_obj - timedelta(days=1)
+# def asdf(before_oneday_obj):
+#     for row_idx, cell in enumerate(report_sheet["B"]):
+#         try:
+#             cv = cell.value.date()
+#         except AttributeError:
+#             continue
+#         if cv == before_oneday_obj:
+                    
+#             break
