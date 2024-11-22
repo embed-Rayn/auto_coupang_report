dd5b94f72dc7a8cabe50509b886d98b10b3c7ba7
embed-Rayn <57181674+embed-Rayn@users.noreply.github.com>
2024-11-22 15:01:47 +0900

    update v2.3

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ import re
 import pandas as pd
 
 
-def date_minus_day_to_str(date_obj, days = 1, str_format = "%Y년 %m월 %d일"):
-    before_oneday_obj = date_obj - timedelta(days=days)
-    formatted_date = before_oneday_obj.strftime(str_format)
+def date_plus_day_to_str(date_obj, days = 1, str_format = "%Y년 %m월 %d일"):
+    after_oneday_obj = date_obj + timedelta(days=days)
+    formatted_date = after_oneday_obj.strftime(str_format)
     return formatted_date
 
 
@@ -47,7 +47,7 @@ class WindowClass(QMainWindow, Ui_Dialog):
         self.btn_search_excel_3.clicked.connect(self.browse_3)
         self.btn_execute.clicked.connect(self.push_execute)
         self.gui_home_dir = os.path.abspath(".")
-
+        self.is_successed = True
 
     def browse_1(self):
         try:
@@ -87,12 +87,11 @@ class WindowClass(QMainWindow, Ui_Dialog):
         self.btn_search_excel_1.setEnabled(True)
         self.btn_search_excel_2.setEnabled(True)
         self.btn_search_excel_3.setEnabled(True)
-        if rst:
+        if rst and self.is_successed:
             self.textEdit_log.setText(f"{self.textEdit_log.toPlainText()}작업이 완료 됐습니다.\n생성한 리포트 파일 위치: {self.output_file_path}\n")
         else:
             self.textEdit_log.setText(f"{self.textEdit_log.toPlainText()}{self.input_file_path_1}sheet가 여러개 입니다. 실행 종료\n")
-        self.isSetInput = False
-        self.isSetOutput = False
+        self.is_successed = True
 
 
     def process_file_path(self, report_day):
@@ -111,6 +110,8 @@ class WindowClass(QMainWindow, Ui_Dialog):
         workbook_dayily = openpyxl.load_workbook(self.input_file_path_1)
         sheet = workbook_dayily.active  # 기본 활성화된 시트를 선택합니다. 특정 시트가 있으면 sheet = workbook["Sheet1"] 과 같이 지정합니다.
         sheets = workbook_dayily.sheetnames
+        ## Excel 데이터 to 리스트
+        ### Daily 
         row_values_daily = []
         for row_idx, row in enumerate(sheet):
             if row_idx == 0:
@@ -118,7 +119,7 @@ class WindowClass(QMainWindow, Ui_Dialog):
             row_data = [col.value for col in row]
             row_values_daily.append(row_data)
         workbook_dayily.close()
-
+        ### total 
         workbook_cum = openpyxl.load_workbook(self.input_file_path_2)
         sheet = workbook_cum.active  # 기본 활성화된 시트를 선택합니다. 특정 시트가 있으면 sheet = workbook["Sheet1"] 과 같이 지정합니다.
         sheets = workbook_cum.sheetnames
@@ -129,7 +130,8 @@ class WindowClass(QMainWindow, Ui_Dialog):
             row_data = [col.value for col in row]
             row_values_cumul.append(row_data)
         workbook_cum.close()
-
+        ## 리스트 to Data
+        ### Daily
         dataset_daily = {}
         for row in row_values_daily:
             date_obj = datetime.strptime(str(int(row[0])), "%Y%m%d").date()
@@ -144,7 +146,7 @@ class WindowClass(QMainWindow, Ui_Dialog):
                 dataset_daily[date_obj].append(temp)
             except KeyError:
                 dataset_daily[date_obj] = [temp]
-
+        ### total 
         dataset_cumul = []
         for row in row_values_cumul:
             temp = {}
@@ -168,33 +170,37 @@ class WindowClass(QMainWindow, Ui_Dialog):
         if len(sheets) != 1:
             return False
         report_day = max(dataset_daily.keys())
-        file_day = report_day - timedelta(days=1)
+        file_day = report_day + timedelta(days=1)
         self.process_file_path(file_day)
-        shutil.copy(self.input_file_path_3, self.output_file_path)
+        try:
+            shutil.copy(self.input_file_path_3, self.output_file_path)
+        except PermissionError:
+            self.textEdit_log.setText(f"{self.textEdit_log.toPlainText()}{self.output_file_path}파일이 이미 존재하거나 열려있어 실패.\n")
+            self.is_successed = False
         #################################################### 리포트 파일
         #################################################### ["요약"] 시트
         report_wb = openpyxl.load_workbook(self.output_file_path)
         report_sheet = report_wb["요약"]
-        date_str = date_minus_day_to_str(report_day, 1, "%Y년 %m월 %d일")
+        date_str = date_plus_day_to_str(report_day, 1, "%Y년 %m월 %d일")
         report_sheet["B31"] = date_str
-        date_str = date_minus_day_to_str(report_day, 1, "%Y-%m-%d")
+        date_str = date_plus_day_to_str(report_day, 1, "%Y-%m-%d")
         report_sheet["C104"] = date_str
         #################################################### ["쿠팡_일일"] 시트
         report_sheet = report_wb["쿠팡_일일"]
         for date_obj in dataset_daily.keys():
-            before_oneday_obj = date_obj - timedelta(days=1)
             for row_idx, cell in enumerate(report_sheet["B"]):
                 try:
                     cv_date = cell.value.date()
                 except AttributeError:
                     continue
-                if cv_date == before_oneday_obj:
+                if cv_date.strftime("%y-%m-%d") == date_obj.strftime("%y-%m-%d"):
                     data_idx = row_idx + 1
                     report_sheet[f"D{data_idx}"] = sum(campaign["exposure_num"] for campaign in dataset_daily[date_obj])
                     report_sheet[f"E{data_idx}"] = sum(campaign["click_num"] for campaign in dataset_daily[date_obj])
                     report_sheet[f"H{data_idx}"] = sum(campaign["ad_cost"] for campaign in dataset_daily[date_obj])
                     report_sheet[f"I{data_idx}"] = sum(campaign["convert_num"] for campaign in dataset_daily[date_obj])
                     report_sheet[f"L{data_idx}"] = sum(campaign["convert_cost"] for campaign in dataset_daily[date_obj])
+                    continue
         #################################################### ["쿠팡_누적"] 시트
         report_sheet = report_wb["쿠팡_누적"]
         key_col_dict_c = {"campaign_name": "C", "exposure_num": "D", "click_num": "E", "ad_cost": "H", "convert_num": "I", "convert_cost": "L"}
